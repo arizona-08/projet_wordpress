7149f68df3ebe3f2a4ecb7594de0021815482f89
PERROUAS Thibault <thibaultperrouas8@gmail.com>
2024-07-21 18:54:40 +0200

    WIP on dev

---
+++ ESGI-theme/services.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Services Page
+*/
+get_header();
+?>
+
+<div class="services-content">
+    <h1><?php the_title(); ?>.</h1>
+</div>
+
+<div class="sections">
+
+</div>
+
+<?php get_footer(); ?>
