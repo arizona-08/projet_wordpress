6be62a6980836d16b2e3a110ef368717065e2b0a
Jonathan Assi <assijonathan2@gmail.com>
2024-07-22 02:21:56 +0200

    fixed template-parts issues

--- ESGI-theme/page-about.php
+++ ESGI-theme/page-about.php
@@ -10,10 +10,10 @@ get_template_part('template-parts/about-us');
     <div class="team-members">
         <?php for ($i = 1; $i <= 4; $i++): ?>
             <div class="team-member">
-                <img src="<?php echo get_theme_mod("team_image_$i", get_template_directory_uri() . "/images/png/team$i.png"); ?>" alt="Team Member <?php echo $i; ?>">
-                <h3><?php echo get_theme_mod("about_us_role_$i", "Role $i"); ?></h3>
-                <p class="phone"><?php echo get_theme_mod("about_us_phone_$i", "+33 1 53 31 25 23"); ?></p>
-                <p class="email"><?php echo get_theme_mod("about_us_email_$i", "email$i@example.com"); ?></p>
+                <img src="<?php echo esc_url(get_theme_mod("team_member{$i}photo", get_template_directory_uri() . "/images/png/team$i.png")); ?>" alt="Team Member <?php echo $i; ?>">
+                <h3><?php echo esc_html(get_theme_mod("team_member{$i}role", "Role $i")); ?></h3>
+                <p class="phone"><?php echo esc_html(get_theme_mod("team_member{$i}phone", "+33 1 53 31 25 23")); ?></p>
+                <p class="email"><?php echo esc_html(get_theme_mod("team_member{$i}_email", "email$i@example.com")); ?></p>
             </div>
         <?php endfor; ?>
     </div>
